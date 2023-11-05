b227f43b96b6e3cee16dd3a5c4aedf66cf3f378b
LahiruHarshana <lharshana2002@gmail.com>
2023-11-05 12:36:02 +0530

    updated

--- src/main/java/lk/ijse/animal_clinic/AppInitializer.java
+++ src/main/java/lk/ijse/animal_clinic/AppInitializer.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class AppInitializer {
+public class AppInitializer extends Application{
     public static void main(String[] args) {
         launch(args);
     }
