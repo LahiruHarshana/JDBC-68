00ee2eea294603818d3db4a52bbdab279f6a2bab
LahiruHarshana <lharshana2002@gmail.com>
2023-11-05 12:35:30 +0530

    updated

--- src/main/java/lk/ijse/animal_clinic/AppInitializer.java
+++ src/main/java/lk/ijse/animal_clinic/AppInitializer.java
@@ -1,4 +1,20 @@
 package lk.ijse.animal_clinic;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 public class AppInitializer {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"))));
+        stage.setTitle("Customer Form");
+        stage.centerOnScreen();
+
+        stage.show();
+    }
 }
--- src/main/java/lk/ijse/animal_clinic/AppInitializerWrapper.java
+++ src/main/java/lk/ijse/animal_clinic/AppInitializerWrapper.java
@@ -1,4 +1,8 @@
 package lk.ijse.animal_clinic;
 
 public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+
 }
